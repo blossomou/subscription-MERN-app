bb010d773ddeaa27678fef195079cf97ea57ed5a
QingOu <qingqingou@gmail.com>
2021-11-22 14:58:54 -0500

    set up mongo db atlas

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
-import authRoutes from './routes/auth'
+import authRoutes from './routes/auth';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
 
-app.use(express.json());
-app.use('/auth', authRoutes);
 
-app.listen(8080, () => {
-    console.log(`Now listening to port 8080`);
-});
+mongoose
+    .connect(
+        process.env.MONGO_URI as string)
+    .then(() => {
+        console.log('Connected to mongo db');
+
+        const app = express();
+        app.use(express.json());
+        app.use('/auth', authRoutes);
+
+        app.listen(8080, () => {
+
+            console.log(`Now listening to port 8080`);
+        });
+    })
+    .catch((error) => {
+        console.log({error})
+        throw new Error(error);
+    })
