8563432cddc90f551d5882bdb06c01caa0880fdb
QingOu <qingqingou@gmail.com>
2021-11-22 12:44:34 -0500

    init check in - styled, bootstrap, modal, server

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Hero from './components/Hero/Hero';
+import Navbar from './components/Nav/Nav';
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
